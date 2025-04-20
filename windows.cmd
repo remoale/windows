99030f1aa318a51b42148708c20dadd6136a2f26
Remo <remoale28@gmail.com>
2025-04-20 00:16:59 -0400

    winget import

--- windows.cmd
+++ windows.cmd
@@ -1,7 +1,7 @@
 @echo off
 cls
 color 07
-title Winget
+title Setup
 mode 76, 30
 set "nul1=1>nul"
 set "nul2=2>nul"
@@ -25,69 +25,170 @@ reg query HKCU\Console /v QuickEdit %nul2% | find /i "0x0" %nul1% || if not defi
 	exit
 )
 
-powershell.exe "cd %~dp0; $f=[io.file]::ReadAllText('%~f0') -Split ':Install\:.*'; Invoke-Expression ($f[1]);" & goto End
+powershell.exe "cd %~dp0; $f=[io.file]::ReadAllText('%~f0') -Split ':Programs\:.*'; Set-Content -Path 'programs.json' -Value ($f[1]); winget import -i 'programs.json' --accept-package-agreements --accept-source-agreements; Remove-Item -Recurse $ENV:USERPROFILE\Desktop\*.lnk"
+del programs.json
 
-:Install:
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+powershell.exe "cd %~dp0; $f=[io.file]::ReadAllText('%~f0') -Split ':Settings\:.*'; Invoke-Expression ($f[1]);" & goto End
 
-$programs = [ordered]@{
-	"Bitwarden" = "Bitwarden";
-	"Microsoft" = @(
-		"Edge",
-		"WindowsTerminal",
-		"VisualStudioCode",
-		"PowerShell",
-		"DevHome",
-		"Office",
-		"OneDrive",
-		"PCManager",
-		"PowerToys",
-		"Sysinternals"
-	);
-	"Notion" = @(
-		"Notion",
-		"NotionCalendar"
-	);
-	"" = @(
-		"9PKTQ5699M62",
-		"9PFHDD62MXS1",
-		"9WZDNCRFJ3TJ"
-	);
-	"Python" = "Python.3.12";
-	"JanDeDobbeleer" = "OhMyPosh";
-	"Git" = "Git";
-	"GitHub" = "GitHubDesktop";
-	"Neovim" = "Neovim";
-	"Gyan" = "FFmpeg";
-	"Discord" = "Discord"
-}
-
-foreach ($key in $programs.Keys) {
-	foreach ($value in $programs[$key]) {
-		if ($key) {
-			winget install "$key.$value" --accept-package-agreements --accept-source-agreements
-		} else {
-			winget install "$value" --accept-package-agreements --accept-source-agreements
+:Programs:
+{
+	"$schema" : "https://aka.ms/winget-packages.schema.2.0.json",
+	"CreationDate" : "2025-04-19T23:05:49.447-00:00",
+	"Sources" : 
+	[
+		{
+			"Packages" : 
+			[
+				{
+					"PackageIdentifier" : "7zip.7zip"
+				},
+				{
+					"PackageIdentifier" : "Git.Git"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.Office"
+				},
+				{
+					"PackageIdentifier" : "Proton.ProtonVPN"
+				},
+				{
+					"PackageIdentifier" : "Zen-Team.Zen-Browser"
+				},
+				{
+					"PackageIdentifier" : "JanDeDobbeleer.OhMyPosh"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2010.x64"
+				},
+				{
+					"PackageIdentifier" : "OpenJS.NodeJS"
+				},
+				{
+					"PackageIdentifier" : "GitHub.cli"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.PowerShell"
+				},
+				{
+					"PackageIdentifier" : "OBSProject.OBSStudio"
+				},
+				{
+					"PackageIdentifier" : "Valve.Steam"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2013.x64"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2012.x86"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.DotNet.DesktopRuntime.8"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2013.x86"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2010.x86"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2015+.x86"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2015+.x64"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCRedist.2012.x64"
+				},
+				{
+					"PackageIdentifier" : "Notion.Notion"
+				},
+				{
+					"PackageIdentifier" : "Discord.Discord"
+				},
+				{
+					"PackageIdentifier" : "Postman.Postman"
+				},
+				{
+					"PackageIdentifier" : "Notion.NotionCalendar"
+				},
+				{
+					"PackageIdentifier" : "Python.Python.3.13"
+				},
+				{
+					"PackageIdentifier" : "Anysphere.Cursor"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.PowerToys"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.AppInstaller"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.UI.Xaml.2.8"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.VCLibs.Desktop.14"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.WindowsTerminal"
+				},
+				{
+					"PackageIdentifier" : "Microsoft.WSL"
+				},
+				{
+					"PackageIdentifier" : "Gyan.FFmpeg"
+				}
+			],
+			"SourceDetails" : 
+			{
+				"Argument" : "https://cdn.winget.microsoft.com/cache",
+				"Identifier" : "Microsoft.Winget.Source_8wekyb3d8bbwe",
+				"Name" : "winget",
+				"Type" : "Microsoft.PreIndexed.Package"
+			}
+		},
+		{
+			"Packages" : 
+			[
+				{
+					"PackageIdentifier" : "9NT1R1C2HH7J"
+				}
+			],
+			"SourceDetails" : 
+			{
+				"Argument" : "https://storeedgefd.dsx.mp.microsoft.com/v9.0",
+				"Identifier" : "StoreEdgeFD",
+				"Name" : "msstore",
+				"Type" : "Microsoft.Rest"
+			}
 		}
-	}
+	],
+	"WinGetVersion" : "1.10.340"
 }
+:Programs:
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+:Settings:
+# File Explorer
+Write-Output "Enabling developer-specific settings..."
 
-choco install -y mingw make
+# Allow running local PowerShell scripts without signing (only for local scripts)
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
-# Make `refreshenv` available right away, by defining the $env:ChocolateyInstall
-# variable and importing the Chocolatey profile module.
-# Note: Using `. $PROFILE` instead *may* work, but isn't guaranteed to.
-$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# Registry path for Explorer settings
+$explorerPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 
-# refreshenv is now an alias for Update-SessionEnvironment
-# (rather than invoking refreshenv.cmd, the *batch file* for use with cmd.exe)
-# This should make git.exe accessible via the refreshed $env:PATH, so that it
-# can be called by name only.
-refreshenv
+# Show hidden files and folders
+Set-ItemProperty -Path $explorerPath -Name "Hidden" -Value 1
 
+# Show system files (and hide the warning)
+Set-ItemProperty -Path $explorerPath -Name "ShowSuperHidden" -Value 1
+
+# Show file name extensions
+Set-ItemProperty -Path $explorerPath -Name "HideFileExt" -Value 0
+
+Write-Output "✅ Developer settings enabled successfully."
+
+# Python
 $libraries = @(
 	"numpy",
 	"pandas",
@@ -115,14 +216,7 @@ Add-Content -Path $PROFILE -Value "oh-my-posh init pwsh | Invoke-Expression"
 $profile7 = "$ENV:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
 New-Item -Type File -Path $profile7 -Force
 Add-Content -Path $profile7 -Value "oh-my-posh init pwsh | Invoke-Expression"
-
-git clone https://github.com/NvChad/starter $ENV:USERPROFILE\AppData\Local\nvim; nvim
-
-Remove-Item "$ENV:USERPROFILE\Desktop\GitHub Desktop.lnk" -Force
-Remove-Item "$ENV:USERPROFILE\Desktop\Discord.lnk" -Force
-
-wsl --install
-:Install:
+:Settings:
 
 :End
 del "%~f0"
